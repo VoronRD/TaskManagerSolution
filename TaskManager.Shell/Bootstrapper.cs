2a06cb820b3a0ffc6968b0c7fcccdb3ce68b263b
Voronin Ilya <voronrd@mail.ru>
2025-03-21 17:36:39 +0300

    First commit

---
+++ TaskManager.Shell/Bootstrapper.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using TaskManager.Shell.ViewModels;
+using TaskManager.Shell.Views;
+
+namespace TaskManager.Shell
+{
+    public class Bootstrapper : PrismBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            // Регистрация MainWindow и его ViewModel
+            containerRegistry.RegisterForNavigation<MainWindow, MainWindowViewModel>("MainWindow");
+        }
+
+        //protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        //{
+        //    base.ConfigureModuleCatalog(moduleCatalog);
+
+        //    // Регистрация модулей
+        //    moduleCatalog.AddModule<TasksModule>();
+        //    moduleCatalog.AddModule<TagsModule>();
+        //    moduleCatalog.AddModule<SettingsModule>();
+        //}
+    }
+}
---
+++ TaskManager.Shell/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace TaskManager.Shell.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private readonly IRegionManager _regionManager;
+
+        public MainWindowViewModel(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+            NavigateCommand = new DelegateCommand<string>(Navigate);
+        }
+
+        public DelegateCommand<string> NavigateCommand { get; }
+
+        private void Navigate(string viewName)
+        {
+            _regionManager.RequestNavigate("MainRegion", viewName);
+        }
+    }
+}
