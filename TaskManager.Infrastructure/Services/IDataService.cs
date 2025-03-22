5251b4cb5f16ccc3ee3f1fa448f0fdc0ef8ec079
AGANETHED <astcvetkov1@chsu.ru>
2025-03-22 10:57:04 +0300

    TaskManager.Infrastructure: adding code to Events and Services

--- TaskManager.Infrastructure/Services/IDataService.cs
+++ TaskManager.Infrastructure/Services/IDataService.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManager.Core.Models;
 
 namespace TaskManager.Infrastructure.Services
 {
-    internal interface IDataService
+    public interface IDataService
     {
+        List<Tasks> LoadTasks();
+        void SaveTasks(List<Tasks> tasks);
+
+        List<Tags> LoadTags();
+        void SaveTags(List<Tags> tags);
     }
 }
